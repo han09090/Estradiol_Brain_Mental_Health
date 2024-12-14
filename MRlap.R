138a7d29dc7ad90edb28d45ce8a1a197c8b7a7dd
han <hannahop@uio.no>
2024-12-14 19:23:14 +0100

    Update MRlap.R

--- MRlap.R
+++ MRlap.R
@@ -4,19 +4,18 @@ library(MRlap)
 library(data.table)
 
 # load data
-gwas_childbirths <- read.delim("path/gwas_child.tsv")
+childbirths <- read.table("path/childbirths.txt", header = TRUE, sep = "", dec = ".")
 bag <- read.table("path/BAG.txt", header = TRUE, sep = "", dec = ".")
 IVs <- read.table("path/IVs_BAG_childbirths", header = TRUE, sep = "", dec = ".")
 # headers should be: chr, rsid, pos, ref, alt, se, p, N
 
-
-result <- MRlap(exposure=gwas_childbirths,exposure_name="gwas_childbirths",outcome=bag,
+# run MRlap using instrumental variables
+result <- MRlap(exposure=childbirths,exposure_name="childbirths",outcome=bag,
                 outcome_name="BAG",
                 ld = "/Users/hannah/Documents/MRlap/eur_w_ld_chr",
                 hm3 = "/Users/hannah/Documents/MRlap/w_hm3.noMHC.snplist",
                 do_pruning = F,
                 user_SNPsToKeep = IVs$SNPs)
-
 
 print(result)
 
