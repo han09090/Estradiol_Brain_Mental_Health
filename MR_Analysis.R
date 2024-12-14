5ae7a2704baa6120bfe61850acf6ad9cf774f61b
han <hannahop@uio.no>
2024-12-14 16:40:28 +0100

    Add files via upload

---
+++ MR_Analysis.R
@@ -0,0 +1,55 @@
+# load packages
+library(dplyr)
+library(TwoSampleMR)
+library(ieugwasr)
+
+# load data
+data_file <- read.csv("path/menopause_depr.csv")
+# this data file has the rsid, beta, se, effect_allele, other_allele, p, eaf, and N
+# of the exposure and outcome for the instrumental variables
+
+# Read outcome data
+outcome <- read_outcome_data(filename = "menopause_depr.csv", sep = ",", snp_col = "rsid",
+                             beta_col = "beta_out", se_col = "se_out",
+                             effect_allele = "effect_out", other_allele = "other_out",
+                             pval_col="p_out", samplesize_col = "N_out")
+
+
+# Read exposure data
+exposure <- read_outcome_data(filename = "menopause_depr.csv", sep = ",", snp_col = "rsid",
+                             beta_col = "beta_exp", se_col = "se_exp",
+                             effect_allele = "effect_exp", other_allele = "other_exp",
+                             pval_col="p_exp", samplesize_col = "N_exp")
+
+
+# Harmonize the data by aligning exposure and outcome data
+data <- harmonise_data(exposure_dat = exp_formatted, outcome_dat = outcome)
+
+
+# Calculate the number of rows in the harmonized dataset
+nrow(data)
+# Perform Mendelian Randomization (MR) analysis
+result <- mr(data)
+# Print the MR analysis result
+print(result)
+
+
+# Cochran's Q for heterogeneity
+mr_heterogeneity(data) 
+
+
+# full report of results
+library(Cairo)
+mr_report(data)
+
+
+# plot
+library(ggplot2)
+plotcont <- mr_scatter_plot(result,data)[[1]] +
+  theme_classic() +
+  xlab("SNP effect on menopause") +
+  ylab("SNP effect on depr") +
+  ggtitle("Menopause (Exposure) & depr (Outcome)")
+ggsave("plot.jpeg",width=10.3,height=4.83)
+
+
